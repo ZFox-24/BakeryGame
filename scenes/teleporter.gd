f6bf510ad5dd31cda3c6779a8f0e5441cb472737
ZFox-24 <foxxo3576@gmail.com>
2025-09-26 12:10:23 +0300

    world building 2

--- scenes/teleporter.gd
+++ scenes/teleporter.gd
@@ -3,6 +3,5 @@ extends Area3D
 @export var location := PackedScene
 
 func _on_body_entered(body: Node3D) -> void:
-	print("AAA")
 	var location = self.location
-	get_tree().change_scene_to_packed(location)
+	get_tree().call_deferred("change_scene_to_packed", location)
