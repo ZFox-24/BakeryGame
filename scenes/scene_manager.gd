9c6ff29c445131ed2a4f4969d4ef7556bbb1100a
Zackfoxdev <zackarreyfox@gmail.com>
2026-05-05 21:29:53 +0300

    bakery interior models optimization

--- scenes/scene_manager.gd
+++ scenes/scene_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
-#@onready var current_scene : String = "res://materials/ground.tscn"
-@onready var current_scene: String = "res://scenes/bakery_interior.tscn"
+@onready var current_scene : String = "res://materials/ground.tscn"
+#@onready var current_scene: String = "res://scenes/bakery_interior.tscn"
 
 func _ready() -> void:
 	get_tree().change_scene_to_file.call_deferred(current_scene)
